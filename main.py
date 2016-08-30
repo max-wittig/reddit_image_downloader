714285b826e2d39a8eda6799311ff18d4f5ae8bf
Max Wittig <max.wittig95@gmail.com>
2016-08-30 14:43:37 +0200

    added command line options

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from reddit_downloader import *
 from sub_reddit import *
+import argparse
 
 
-def get_subreddits():
+def get_subreddits(subreddit_names, limit=100):
     subreddits = []
-    sub = Subreddit("funny")
-    sub.limit = 1000
-    subreddits.append(sub)
-    sub2 = Subreddit("pics")
-    sub2.limit = 1000
-    subreddits.append(sub2)
+    for name in subreddit_names:
+        sub = Subreddit(name)
+        sub.limit = limit
+        subreddits.append(sub)
     return subreddits
+
+
+def get_args():
+    parser = argparse.ArgumentParser("Downloads images from reddit")
+    parser.add_argument("-d", "--debug", help="Use Debug option", action='store_true')
+    parser.add_argument("-s", "--subreddits", help="Add subreddits to download from", nargs="+")
+    parser.add_argument("-l", "--limit", help="How many submission it should look at")
+
+    options = parser.parse_args()
+    return vars(options)
+
+
+def debug_get_subreddits():
+    return ["funny", "pics"]
 
 
 def get_user_agent():
@@ -18,7 +31,22 @@ def get_user_agent():
 
 
 def main():
-    reddit_downloader = RedditDownloader(get_subreddits())
+    subreddit_names = []
+    options = get_args()
+    debug = options.get("debug")
+    arg_subreddits = options.get("subreddits")
+    limit = options.get("limit")
+    if debug:
+        subreddit_names = debug_get_subreddits()
+    else:
+        subreddit_names = arg_subreddits
+
+    if limit is None:
+        limit = 100
+    if subreddit_names is None:
+        exit("No arguments")
+    subreddits = get_subreddits(subreddit_names, limit=limit)
+    reddit_downloader = RedditDownloader(subreddits)
     reddit_downloader.download()
 
 if __name__ == '__main__':
